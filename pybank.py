e88d9b613dfdb5f9fc57b5da48806c8818b87dcf
urmilshroff <urmilshroff@gmail.com>
2018-08-21 01:04:16 +0530

    Updated

--- pybank.py
+++ pybank.py
@@ -1,14 +1,27 @@
 import sys
+import shlex,subprocess
+import time
 
 try:
     import kivy
     kivy.require("1.10.0")
     from kivy.app import App
     from kivy.uix.label import Label
+    import pyrebase
     dependencies=True
     
 except ImportError:
-    print("\nSorry, you don't seem to have Kivy installed for the following version of Python: {}\n\nYou can try by running 'pip3 install kivy'.\n".format(sys.version))
+    print("\nSorry, you don't seem to have the required dependencies installed for the following version of Python: {}\n".format(sys.version))
+    
+    if input("Would you like to install Kivy? Y/N:\n")=="y":
+        subprocess.Popen(shlex.split("pip3 install Kivy"))
+        time.sleep(3)
+        
+    if input("Would you like to install Pyrebase? Y/N:\n")=="y":
+        subprocess.Popen(shlex.split("pip install Pyrebase"))
+        time.sleep(3)
+        
+    print("Please restart the program")
     dependencies=False
     
 if dependencies==True:
