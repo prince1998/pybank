636c986ca406531f07b393630031593c8c24a6fe
urmilshroff <urmilshroff@gmail.com>
2018-08-27 15:23:13 +0530

    Cleaned up code

---
+++ pyrebase_test.py
@@ -0,0 +1,22 @@
+import pyrebase
+
+config={
+    #Enter your config details
+  }
+
+firebase=pyrebase.initialize_app(config)
+auth=firebase.auth()
+user=auth.sign_in_with_email_and_password('testuser@gmail.com','password')
+db=firebase.database()
+
+data={"name":"Test User"}
+db.child("users").child("User1").set(data)
+db.child("users").child("User1").update({"Balance":50})
+
+data={"name":"Another User"}
+db.child("users").child("User2").set(data)
+db.child("users").child("User2").update({"Balance":150})
+
+# Execute these two lines of code to change balances
+# db.child("users").child("User1").update({"Balance":150})
+# db.child("users").child("User2").update({"Balance":100})
