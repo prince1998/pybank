2b382879077b47f2ee71778b5ad93c34af2c7da6
urmilshroff <urmilshroff@gmail.com>
2018-09-10 22:51:42 +0530

    Cleaned up files

--- pybank.py
+++ pybank.py
@@ -4,26 +4,64 @@ import time
 
 try:
     import kivy
-    kivy.require("1.10.0")
     from kivy.app import App
-    from kivy.uix.label import Label
+    from kivy.lang import Builder
+    from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+    from kivy.properties import ObjectProperty,StringProperty
     import pyrebase
-    dependencies=True
+    
+    dependencies_exist=True #if all packages are installed
     
 except ImportError:
     print("\nSorry, you don't seem to have the required dependencies installed for the following version of Python: {}\n".format(sys.version))
     
     if input("Would you like to install Kivy? Y/N:\n")=="y":
-        subprocess.Popen(shlex.split("pip3 install Kivy"))
-        time.sleep(3)
+        subprocess.Popen(shlex.split("pip3 install Kivy")) #installs Kivy
         
     if input("Would you like to install Pyrebase? Y/N:\n")=="y":
-        subprocess.Popen(shlex.split("pip install Pyrebase"))
-        time.sleep(3)
+        subprocess.Popen(shlex.split("pip install Pyrebase")) #installs Pyrebase
         
     print("Please restart the program")
     dependencies=False
     
-if dependencies==True:
-    print("Hello, world!")
-    #insert code here
+if dependencies:
+    
+    class ScreenManagement(ScreenManager):
+        pass
+              
+    class RegisterScreen(Screen):
+        pass
+
+    class LoginScreen(Screen):
+        def validate(self):
+            self.email = self.ids.email.text
+            self.password = self.ids.password.text
+            obj = SetGetLoginInfo()
+            obj.setLoginInfo(self.email)
+            self.manager.current = 'user' 
+                            
+    class UserScreen(Screen):
+        def getname(self):
+            obj = SetGetLoginInfo()
+            self.ids.label.text = obj.getLoginInfo()
+    
+    class SetGetLoginInfo:
+        email_id = ""
+
+        def __init__(self):
+            pass
+
+        def setLoginInfo(self,email):
+            SetGetLoginInfo.email_id = email
+
+        def getLoginInfo(self):
+            return SetGetLoginInfo.email_id
+    
+    presentation = Builder.load_file('layouts.kv')
+    
+    class PyBank(App):
+        def build(self):
+            return presentation
+    
+    if __name__ == '__main__':
+        PyBank().run()
