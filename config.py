0a88c98762b0c3784493b09b1441a91cd14f3ecf
Akhil <akhilkshah@gmail.com>
2018-09-29 20:30:07 +0530

    Added most of the functionalities
    The following things are done -
    Register/Sign In
    Adding/Transfering Money
    Program does not handle errors yet.

---
+++ config.py
@@ -0,0 +1,55 @@
+import pyrebase
+
+config = {
+          #Your Details
+};
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth();
+db = firebase.database();
+
+class Config:
+
+          def __init__(self):
+                    pass
+
+          def create_user(self,name,email,password):
+                    user = auth.create_user_with_email_and_password(email, password)
+                    email = email.split('@')[0]
+                    db.child("users").child(email).set({"name":name,"balance":0})
+                    db.child("users").child(email).child("messages").push("Your Transactions - ")
+
+          def sign_in_user(self,email,password):
+                    user = auth.sign_in_with_email_and_password(email, password)
+
+          def get_user(self,email):
+                    email = email.split('@')[0]
+                    name = db.child("users").child(email).child("name").get()
+                    balance = db.child("users").child(email).child("balance").get()
+                    message = db.child("users").child(email).child("message").get()
+                    return name.val(),balance.val(),message.val()
+
+          def add_money(self,amount,email):
+                    email = email.split('@')[0]
+                    balance = db.child("users").child(email).child("balance").get()
+                    amount = amount + balance.val()
+                    db.child("users").child(email).update({"balance":amount})
+
+          
+          def transfer_money(self,amount,user_email,transfer_email):
+                    
+                    user_email = user_email.split('@')[0]
+                    transfer_email = transfer_email.split('@')[0]
+                    
+                    balance = db.child("users").child(user_email).child("balance").get()
+                    amount_user = balance.val() - amount
+                    db.child("users").child(user_email).update({"balance":amount_user})
+
+                    balance = db.child("users").child(transfer_email).child("balance").get()
+                    amount_transfer = amount + balance.val()
+                    db.child("users").child(transfer_email).update({"balance":amount_transfer})
+                    
+                    
+
+          
+          
--- pybank.py
+++ pybank.py
@@ -8,6 +8,7 @@ try:
     from kivy.lang import Builder
     from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
     from kivy.properties import ObjectProperty,StringProperty
+    from kivy.uix.popup import Popup
     
     import pyrebase
     
@@ -31,22 +32,67 @@ if dependencies_exist: #only if everything is installed
         pass
     
     class RegisterScreen(Screen):
-        pass
+              
+        def create_user(self):
+            self.name = self.ids.name.text
+            self.email = self.ids.email.text
+            self.password = self.ids.password.text
+            cnfig = config.Config()
+            cnfig.create_user(self.name,self.email,self.password)
+        
 
     class LoginScreen(Screen):
         def validate(self):
             self.email = self.ids.email.text
             self.password = self.ids.password.text
+            cnfig = config.Config()
+            cnfig.sign_in_user(self.email,self.password)
             obj = SetGetLoginInfo()
             obj.setLoginInfo(self.email)
             self.manager.current = 'user' 
                             
     class UserScreen(Screen):
-        def getname(self):
+
+        def display(self):
             obj = SetGetLoginInfo()
-            self.ids.label.text = obj.getLoginInfo()
+            email = obj.getLoginInfo()
+            cnfig = config.Config()
+            name,balance,message = cnfig.get_user(email)
+            self.balance = str(balance)
+            self.ids.name.text = "Welcome "+name
+            self.ids.balance.text = "Balance = "+str(balance)
+        
+
+    class MoneyScreen(Screen):
+        pass
+
+
+
+    class AddMoney(Popup):
+
+        def add_money(self):
+            obj = SetGetLoginInfo()
+            email = obj.getLoginInfo()
+            amount = int(self.ids.money_add.text)
+            cnfig = config.Config()
+            cnfig.add_money(amount,email)
+
     
+            
+    class TransferMoney(Popup):
+
+        def transfer_money(self):
+            obj = SetGetLoginInfo()
+            user_email = obj.getLoginInfo()
+            transfer_email = self.ids.transfer_email.text
+            amount = int(self.ids.money_transfer.text)
+            cnfig = config.Config()
+            cnfig.transfer_money(amount,user_email,transfer_email)
+
+    
+  
     class SetGetLoginInfo:
+              
         email_id = ""
 
         def __init__(self):
